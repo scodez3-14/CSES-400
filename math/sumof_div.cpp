881c0cdee9fffd7cd9c4f743a3becee8a6c63a0f
scodez3-14 <santupii3.1@gmail.com>
2025-11-12 18:39:53 +0530

    finally

--- math/sumof_div.cpp
+++ math/sumof_div.cpp
@@ -14,24 +14,35 @@ void logger(string vars, Args&&... values) {
     (..., (cout << delim << values, delim = ", "));
     cout<<endl;
 }
-int mod=1e9+7;
 
-
-void solve(){
-    int n;
+void solve() {
+    long long n;
     cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) a[i] = i + 1;
+    const long long mod = 1e9 + 7;
 
-    map<int,int> cnt;
-    for(int i : a) cnt[i]++;
+    vector<long long> a, b;
+    long long i = 1;
+    b.push_back(i);
 
-    ll ans = 0;
-    for(int g = n; g >= 1; g--){
-        for(int i = g; i <= n; i += g){
-            ans = (ans + 1LL * cnt[g] * g) % mod;  
-        }
+    while (i <= n) {
+        long long q = n / i;
+        long long next = n / q + 1;
+        a.push_back(q);
+        b.push_back(next);
+        i = next;
     }
+
+    long long ans = 0;
+    for (int i = 0; i + 1 < (int)b.size(); i++) {
+        long long l = b[i];
+        long long r = b[i + 1] - 1;
+
+        long long len = (r - l + 1) % mod;
+        long long sum_lr = ((l + r) % mod) * len % mod * ((mod + 1) / 2) % mod; 
+
+        ans = (ans + (a[i] % mod) * sum_lr % mod) % mod;
+    }
+
     cout << ans << endl;
 }
 
@@ -42,7 +53,7 @@ int main() {
     // cin >> t;
     while (t--) {
        solve(); 
-    }
+    } 
 
     return 0;
 }
